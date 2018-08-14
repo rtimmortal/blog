32e919ba27d94b315044dd5dd74d74d979c963db
tengteng <tengteng@codemao.cn>
2018-08-14 17:02:17 +0800

    feat():react-dom

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('index'));
+app.use(express.static('src'));
 
 app.listen(3200, function () {
   console.log('Example app listening on port 3200!');
